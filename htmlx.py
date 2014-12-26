0481b8743ebc6fb623e29afc05dc58ffdf41bdca
Benjamin Althues <benjamin@babab.nl>
2014-12-26 01:21:13 +0100

    Add close() method; small refactorings
    Add close() method to close all open nodes at the end. Eliminating the
    need for a last line in the test.htmlx example.

--- htmlx.py
+++ htmlx.py
@@ -5,51 +5,53 @@ SETTING_INDENT_SPACES = 2
 
 class Parser:
     def __init__(self):
+        '''Initialize some instance variables'''
         self.data = []
+        self.latest_node = 0
         self.lineno = 0
         self.nodelvl = 0
-        self.nodes = []
-        self.latest_node = 0
+        self.parsed = []
 
         for i in range(0, 100):
-            self.nodes.append(None)
+            self.parsed.append(None)
 
     def file(self, filename):
+        '''Parse a file, this will simply call parseLine() for each line'''
         with open(filename) as f:
             for line in f:
                 self.parseLine(line)
+        return self
 
     def parseLine(self, line):
+        '''Parse a single line'''
         self.lineno += 1
         indentlvl = int((len(line) - len(line.lstrip()))
                         / SETTING_INDENT_SPACES)
-        stripped = line.lstrip().split()
+        content = line.lstrip().split()
 
         # Skip empty lines and comment lines
-        if not stripped or (indentlvl == 0 and stripped[0].startswith('#')):
+        if not content or (indentlvl == 0 and content[0].startswith('#')):
             return self
 
-        self.registerNode(indentlvl, stripped[0])
+        self._registerNode(indentlvl, content[0])
 
         self.data.append({
             'line': self.lineno,
             'indentlvl': indentlvl,
-            'stripped': stripped,
+            'content': content,
         })
+        return self
 
-    def registerNode(self, indentlvl, element):
+    def close(self):
+        '''Close all open nodes'''
+        self._closeNodes(0)
+        return self
+
+    def _registerNode(self, indentlvl, element):
         if indentlvl < self.nodelvl:
-            self.nodes[self.latest_node] = None
+            self._closeNodes(indentlvl)
 
-            for i in range(99, indentlvl - 1, -1):
-                if self.nodes[i]:
-                    print('{}</{}>'.format(
-                        ((' ' * SETTING_INDENT_SPACES) * i),
-                        self.nodes[i]
-                    ))
-                    self.nodes[i] = None
-
-        self.nodes[indentlvl] = element
+        self.parsed[indentlvl] = element
         self.latest_node = indentlvl
 
         print('{}<{}>'.format(
@@ -57,10 +59,16 @@ class Parser:
         ))
         self.nodelvl = indentlvl
 
-if __name__ == '__main__':
-    # import yaml
+    def _closeNodes(self, indentlvl):
+        self.parsed[self.latest_node] = None
+        for i in range(99, indentlvl - 1, -1):
+            if self.parsed[i]:
+                print('{}</{}>'.format(
+                    ((' ' * SETTING_INDENT_SPACES) * i),
+                    self.parsed[i]
+                ))
+                self.parsed[i] = None
 
+if __name__ == '__main__':
     p = Parser()
-    p.file('test.htmlx')
-    print(p.nodes)
-    # print(yaml.dump(p.data))
+    p.file('test.htmlx').close()
